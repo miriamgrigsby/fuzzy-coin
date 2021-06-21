3a259a08eea71b0cdbe23b3719384e45d46e0282
Miriam Grigsby <miriamgrigsby@gmail.com>
2021-06-20 19:52:25 -0600

    Chore: add black button text

--- src/App.tsx
+++ src/App.tsx
@@ -35,27 +35,31 @@ export const App = () => {
         </div>
         <nav className={'headerNavigationContainer'}>
           <button className="mainSiteLink">
-            <a href="https://fuzzy.finance" className="mainLink">
+            <a href="https://fuzzy.finance" className="mainLink" style={{ color: 'black' }}>
               Website
             </a>
           </button>
           <button className="mainSiteLink">
-            <a href="https://fuzzyinu.medium.com/" className="mainLink">
+            <a href="https://fuzzyinu.medium.com/" className="mainLink" style={{ color: 'black' }}>
               Terminology
             </a>
           </button>
           <button className="mainSiteLink">
-            <a href="https://t.me/fuzzyinu" className="mainLink">
+            <a href="https://t.me/fuzzyinu" className="mainLink" style={{ color: 'black' }}>
               Telegram
             </a>
           </button>
           <button className="mainSiteLink">
-            <a href="http://dextools.io/app/uniswap/pair-explorer/0x513f6bb1d10b06315cf130e452fcc7da51a9318f" className="mainLink">
+            <a
+              href="http://dextools.io/app/uniswap/pair-explorer/0x513f6bb1d10b06315cf130e452fcc7da51a9318f"
+              className="mainLink"
+              style={{ color: 'black' }}
+            >
               Chart
             </a>
           </button>
           <button className="mainSiteLink">
-            <a href="https://fuzzy.finance/whitepaper" className="mainLink">
+            <a href="https://fuzzy.finance/whitepaper" className="mainLink" style={{ color: 'black' }}>
               Whitepaper
             </a>
           </button>
